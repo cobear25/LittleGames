8d79a33854edaebc98008c39c9d5ab60665fd84a
Cody Mace <codymace@gmail.com>
2019-10-11 07:53:55 -0600

    Add Alchemy

---
+++ Assets/Alchemy/AlchemyGameController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AlchemyGameController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject gameOverPanel;
+    [SerializeField]
+    Text scoreText;
+    [SerializeField]
+    Text successText;
+    [SerializeField]
+    Text timerText;
+    [SerializeField]
+    AlchemyObject fixedObject;
+    [SerializeField]
+    AlchemyObject changeableObject;
+
+    [HideInInspector]
+    public bool isGameOver;
+    int score = 0;
+
+    float timeRemaining = 6.0f;
+    bool timerStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+        UpdateUI();
+        Invoke("RandomizeFixedObject", 1.0f);
+    }
+
+    void Update()
+    {
+        if (timerStarted) {
+            timeRemaining -= Time.deltaTime;
+            timerText.text = timeRemaining.ToString("0.00");
+            if (timeRemaining <= 0) {
+                timerStarted = false;
+                GameOver();
+            }
+        }
+    }
+
+    void StartTimer() {
+        timeRemaining = 6.0f;
+        timerStarted = true;
+    }
+
+    void RandomizeFixedObject() {
+        StartTimer();
+        changeableObject.ResetObject();
+        fixedObject.ResetObject();
+        for (int i = 0; i < 10; i++)
+        {
+            int randomAction = Random.Range(0, 6);
+            switch (randomAction) {
+                case 0:
+                    fixedObject.ColorBlue();
+                    break;
+                case 1:
+                    fixedObject.ColorGreen();
+                    break;
+                case 2:
+                    fixedObject.ColorRed();
+                    break;
+                case 3:
+                    fixedObject.Grow();
+                    break;
+                case 4:
+                    fixedObject.Shrink();
+                    break;
+                case 5:
+                    fixedObject.Transform();
+                    break;
+            }
+        }
+    }
+
+    void GameOver() {
+        isGameOver = true;
+        gameOverPanel.SetActive(true);
+    }
+
+    void Success() {
+        if (isGameOver) return;
+        timerStarted = false;
+        score++;
+        successText.enabled = true;
+        UpdateUI();
+        Invoke("HideSuccessText", 0.5f);
+        Invoke("RandomizeFixedObject", 0.5f);
+    }
+
+    void UpdateUI() {
+        scoreText.text = $"Score: {score}";
+    }
+
+    public void ObjectChanged() {
+        if (changeableObject.color == fixedObject.color && 
+            changeableObject.size == fixedObject.size &&
+            changeableObject.currentSprite == fixedObject.currentSprite) {
+                Success();
+            } 
+    }
+
+    void HideSuccessText() {
+        successText.enabled = false; 
+    }
+    
+    public void PlayAgain() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit() {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+}
---
+++ Assets/Alchemy/AlchemyObject.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// cool website for checking color blindness http://www.color-blindness.com/coblis-color-blindness-simulator/
+
+public class AlchemyObject : MonoBehaviour
+{
+    public Sprite rockSprite;
+    public Sprite waterSprite;
+    public Sprite gemSprite;
+
+    public int color = 0;
+    public float size = 0;
+    public int currentSprite = 1;
+
+    public AlchemyGameController gameController;
+
+    SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = rockSprite;
+        Invoke("StartTimer", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ResetObject()
+    {
+        sr.color = Color.white;
+        color = 0;
+        sr.sprite = rockSprite;
+        currentSprite = 1;
+        transform.localScale = new Vector3 (1, 1, 1);
+    }
+
+    public void ColorGreen() {
+        if (gameController.isGameOver) return;
+        sr.color = Color.green;
+        color = 1;
+        gameController.ObjectChanged();
+    }
+
+    public void ColorRed() {
+        if (gameController.isGameOver) return;
+        sr.color = Color.red;
+        color = 2;
+        gameController.ObjectChanged();
+    }
+
+    public void ColorBlue() {
+        if (gameController.isGameOver) return;
+        sr.color = Color.cyan;
+        color = 3;
+        gameController.ObjectChanged();
+    }
+
+    public void Grow() {
+        if (gameController.isGameOver) return;
+        transform.localScale *= 1.2f;
+        size = transform.localScale.magnitude;
+        gameController.ObjectChanged();
+    }
+
+    public void Shrink() {
+        if (gameController.isGameOver) return;
+        transform.localScale /= 1.2f;
+        size = transform.localScale.magnitude;
+        gameController.ObjectChanged();
+    }
+
+    public void Transform() {
+        if (gameController.isGameOver) return;
+        if (sr.sprite == rockSprite) {
+            sr.sprite = waterSprite;
+            currentSprite = 2;
+        } else if (sr.sprite == waterSprite) {
+            sr.sprite = gemSprite;
+            currentSprite = 3;
+        } else {
+            sr.sprite = rockSprite;
+            currentSprite = 1;
+        }
+        gameController.ObjectChanged();
+    }
+}
