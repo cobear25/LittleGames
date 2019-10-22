40aa92813fe624d804d0d36be9ca3168c377b213
Cody Mace <codymace@gmail.com>
2019-10-21 20:00:15 -0600

    Add Hocus Pocus

---
+++ Assets/HocusPocus/HocusPocusGameController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HocusPocusGameController : GameController
+{
+    public GameObject treePrefab;
+    public GameObject pumpkinPrefab;
+
+    List<GameObject> obstacles = new List<GameObject>{};
+
+    int score = 0;
+    int lives = 3;
+
+    void Start()
+    {
+        base.Initialize(); 
+        AddObstacle();
+        UpdateUI();
+        Invoke("AddPoint", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<GameObject> toDeleteList = new List<GameObject> { };
+        foreach (GameObject obstacle in obstacles)
+        {
+            obstacle.transform.Translate(0, -1.5f, 0, Space.World);
+            // Add obstacle to delete list once it's past so we aren't holding on to hundreds of objects
+            if (obstacle.transform.position.y < -100)
+            {
+                toDeleteList.Add(obstacle);
+            }
+        }
+        foreach (GameObject obstacle in toDeleteList) {
+            obstacles.Remove(obstacle);
+            Destroy(obstacle);
+        }
+    }
+
+    void UpdateUI() {
+        scoreText.text = $"Score: {score}";
+    }
+
+    void AddPoint() {
+        score++;
+        UpdateUI();
+        Invoke("AddPoint", 1);
+    }
+
+    void AddObstacle() {
+        if (isGameOver) return;
+        float randomX = Random.Range(-70.0f, 70.0f);
+        if (Random.Range(0, 2) == 0) {
+            GameObject tree = Instantiate(treePrefab, new Vector2(randomX, 60), Quaternion.identity);
+            obstacles.Add(tree);
+        } else {
+            GameObject pumpkin = Instantiate(pumpkinPrefab, new Vector2(randomX, 60), Quaternion.identity);
+            obstacles.Add(pumpkin);
+        }
+        Invoke("AddObstacle", Random.Range(0.5f, 1.5f));
+    }
+
+    public void PlayerHit() {
+        lives--;
+        if (lives <= 0) {
+            GameOver();
+        }
+    }
+}
---
+++ Assets/HocusPocus/HocusPocusPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HocusPocusPlayer : MonoBehaviour
+{
+    public HocusPocusGameController gameController;
+
+    public float delay = 0.0f;
+
+    float speed = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        StartCoroutine(Move(horizontal, delay));
+
+        if (gameController.isGameOver) {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Move(float horizontal, float delayTime) {
+        yield return new WaitForSeconds(delayTime);
+        transform.Translate(Vector2.right * horizontal * speed, Space.World);
+        if (horizontal > 0) {
+            Quaternion targetRotation = new Quaternion(0, 0, -0.1f, transform.rotation.w);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 100 * Time.deltaTime);
+        } else if (horizontal < 0) {
+            Quaternion targetRotation = new Quaternion(0, 0, 0.1f, transform.rotation.w);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 100 * Time.deltaTime);
+        } else {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.identity, 100 * Time.deltaTime);
+        }
+    }
+
+    void Hit() {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        Invoke("EndFlash", 0.2f);
+    }
+
+    void EndFlash() {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Hit();
+        gameController.PlayerHit();
+    }
+
+    void OnBecameInvisible()
+    {
+        Hit();
+        gameController.PlayerHit();
+    }
+}
